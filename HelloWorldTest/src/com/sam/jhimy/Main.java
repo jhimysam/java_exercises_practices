e6232b3deb7bf5b1440904d5afa30b42480a0ba4
jhimysam <jhimysam@gmail.com>
2021-06-19 13:08:21 -0400

    simple Hello World Test

---
+++ HelloWorldTest/src/com/sam/jhimy/Main.java
@@ -0,0 +1,10 @@
+package com.sam.jhimy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // it is a hello world test
+        System.out.println("Hello Wolrd Test");
+
+    }
+}
